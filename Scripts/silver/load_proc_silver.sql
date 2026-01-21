13a3a71048c2fdfda90f1aff664e4cd5ffceabe7
Faris Zulkefli <fariszul16@gmail.com>
2026-01-21 17:02:54 +0800

    Update load_proc_silver.sql

--- Scripts/silver/load_proc_silver.sql
+++ Scripts/silver/load_proc_silver.sql
@@ -78,37 +78,33 @@ BEGIN
 		PRINT '>>> Truncating Table: silver.crm_prd_info'
 		TRUNCATE TABLE silver.crm_prd_info;
 		PRINT '>>> Inserting Data Into: silver.crm_prd_info'
-		INSERT INTO silver.crm_cust_info (
-			cst_id,
-			cst_key,
-			cst_firstname,
-			cst_lastname,
-			cst_marital_status,
-			cst_gndr,
-			cst_create_date
-		)
+			INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt )
 		SELECT
-			cst_id,
-			cst_key,
-			TRIM(cst_firstname) AS cst_firstname,
-			TRIM(cst_lastname) AS cst_lastname,
-			CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-				ELSE 'n/a'
-			END cst_marital_status, -- Normalize marital status to readable format
-			CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-				ELSE 'n/a' 
-			END cst_gndr,  -- Normalize gender to readable format
-			cst_create_date
-		FROM (
-		SELECT 
-		*,
-		ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-		FROM bronze.crm_cust_info
-		WHERE cst_id IS NOT NULL
-		)t
-		WHERE flag_last = 1; -- Selecting the most recent record per customer
+			prd_id,
+			REPLACE(SUBSTRING(prd_key,1,5),'-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,		-- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost, -- Replace nulls value to zero
+			CASE UPPER(TRIM(prd_line))
+				 WHEN 'M' THEN 'Mountain'
+				 WHEN 'R' THEN 'Road'
+				 WHEN 'S' THEN 'other Sales'
+				 WHEN 'T' THEN 'Touring'
+				 ELSE 'n/a'
+			END AS prd_line,	-- Map product line codes to a readable format
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 
+				AS DATE) AS prd_end_dt -- Calculate end date to one day before the next	start date
+		FROM bronze.crm_prd_info;
 		SET @end_time = GETDATE();
 		PRINT '>>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>>-----------------------------------------------------------------';
